2d226b9f3dcc17828269fe32d1020f69091ab566
jeterian <jeterian@gmail.com>
2017-04-17 17:40:54 -0400

    cleaning up positioning

--- js/script.js
+++ js/script.js
@@ -2,88 +2,96 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
 // array of JS objects for the individual quotes and the various pieces of information
+// Array of quotes to display
+
 var quotes = [
   {
     quote: "What I say is, a town isn’t a town without a bookstore. It may call itself a town, but unless it’s got a bookstore, it knows it’s not foolin’ a soul.",
     source: "Neil Gaiman",
     citation: "American Gods",
     year: 2001,
-    tags: "books, life, reading, community"
+    tags: "books, life, reading, community",
+    link: "https://www.goodreads.com/quotes/53787-what-i-say-is-a-town-isn-t-a-town-without"
   },
   {
     quote: "I want to stand as close to the edge as I can without going over. Out on the edge you see all kinds of things you can't see from the center.",
     source: "Kurt Vonnegut",
     citation: "Player Piano",
     year: 1952,
-    tags: "life, inspiration, daring"
+    tags: "life, inspiration, daring",
+    link: "https://www.goodreads.com/quotes/12428-i-want-to-stand-as-close-to-the-edge-as"
   },
   {
     quote: "We're a government that believes in everybody having the illusion of free will.",
     source: "Anthony Burgess",
     citation: "The Wanting Seed",
     year: 1952,
-    tags: "government, citizens, illusion"
+    tags: "government, citizens, illusion",
+    link: "https://www.goodreads.com/quotes/230767-we-re-a-government-that-believes-in-everybody-having-the-illusion"
   },
   {
     quote: "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding.",
     source: "Chimamanda Ngozi Adichie",
     citation: "Americanah",
     year: 2013,
-    tags: "questions, learning, community"
+    tags: "questions, learning, community",
+    link: "https://www.goodreads.com/quotes/919029-if-you-don-t-understand-ask-questions-if-you-re-uncomfortable-about"
   },
   {
     quote: "This was love: a string of coincidences that gathered significance and became miracles.",
     source: "Chimamanda Ngozi Adichie",
     citation: "Half of a Yellow Sun",
     year: 2006,
-    tags: "love"
+    tags: "love",
+    link: "https://www.goodreads.com/quotes/297205-this-was-love-a-string-of-coincidences-that-gathered-significance"
   },
   {
     quote: "It's like everyone tells a story about themselves inside their own head. Always. All the time. That story makes you what you are. We build ourselves out of that story.",
     source: "Patrick Rothfuss",
     citation: "The Name of the Wind",
     year: 2007,
-    tags: "stories, life"
+    tags: "stories, life",
+    link: "https://www.goodreads.com/quotes/20603-it-s-like-everyone-tells-a-story-about-themselves-inside-their"
   },
   {
     quote: "We were the people who were not in the papers. We lived in the blank white spaces at the edges of print. It gave us more freedom. We lived in the gaps between the stories.",
     source: "Margaret Atwood",
     citation: "The Handmaid's Tale",
     year: 1985,
-    tags: "invisibility, life, community"
+    tags: "invisibility, life, community",
+    link: "https://www.goodreads.com/quotes/176687-we-were-the-people-who-were-not-in-the-papers"
   },
   {
     quote: "People who deny the existence of dragons are often eaten by dragons. From within.",
     source: "Ursula Le Guin",
     citation: "The Wave in the Mind: Talks & Essays on the Writer, the Reader & the Imagination",
     year: 2004,
-    tags: "imagination, life, writing, creativity"
+    tags: "imagination, life, writing, creativity",
+    link: "https://www.goodreads.com/quotes/96119-people-who-deny-the-existence-of-dragons-are-often-eaten"
   },
   {
     quote: "The books that the world calls immoral are books that show the world its own shame.",
     source: "Oscar Wilde",
     citation: "The Picture of Dorian Gray",
     year: 1890,
-    tags: "books, life, reading, morality"
+    tags: "books, life, reading, morality",
+    link: "https://www.goodreads.com/quotes/5299-the-books-that-the-world-calls-immoral-are-books-that"
   },
   {
     quote: "We can only see a short distance ahead, but we can see plenty there that needs to be done.",
     source: "Alan Turing",
     citation: "Computing machinery and intelligence",
     year: 1950,
-    tags: "progress, work, vision"
+    tags: "progress, work, vision",
+    link: "https://en.wikiquote.org/wiki/Alan_Turing"
   }
 ];
 
 // array to store quotes that have been shown
 var shownQuo = [];
 
-
-
 // create function named getRandomQuote() which will select a random quote from the above array
-
 function getRandomQuote() {
 
   // random number generator for quotes
